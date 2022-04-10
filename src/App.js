d7d82466dea29996a2f64e4b6dee9269f021c5e7
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-10 08:10:04 +0200

    ADD useEffect so when the component loads

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,12 +9,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(error) //may clear prev erros
 
     try {
-      const response = await fetch('https://swapi.dev/api/filmss/')
+      const response = await fetch('https://swapi.dev/api/films/')
       if (!response.ok) {
         throw new Error("somthing Went Wrong");
       }
@@ -35,7 +35,13 @@ function App() {
 
     }
     setIsLoading(false)
-  }
+  }, [error])
+
+
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
 
 
   let content = <p>Found no Movies.</p>
